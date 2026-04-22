ce21602350f08732d282dc99b204b8220222c7c6
David <david.arenas@automattic.com>
2026-04-22 22:45:44 +0200

    Migrate JS block consumers to new surface

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-collection/frontend.ts
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-collection/frontend.ts
@@ -208,7 +208,7 @@ const productCollectionStore = {
 			const { collection } =
 				getContext< ProductCollectionStoreContext >();
 
-			const productId = productsState.product?.id;
+			const productId = productsState.parentProductInContext?.id;
 
 			if ( productId ) {
 				triggerViewedProductEvent( { collection, productId } );
